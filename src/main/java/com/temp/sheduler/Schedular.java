42280179cad7f6016d17e72a4613af6f2150149e
alessferns <alessferns@gmail.com>
2022-11-16 21:28:01 -0300

    Initial commit

---
+++ src/main/java/com/temp/sheduler/Schedular.java
@@ -0,0 +1,43 @@
+package com.temp.sheduler;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Configuration
+@Service
+@EnableScheduling
+@Controller
+
+
+
+public class Schedular {
+
+	@Autowired
+	private AmqpTemplate amqpTemplate;
+
+	@Value("${livetemperature.rabbitmq.exchange}")
+	private String exchange;
+		
+	@Value("${livetemperature.rabbitmq.routingkey}")
+	private String routingkey;
+
+	@Scheduled(cron ="0/10 * * * * ?")
+	void sendmessage() {
+		System.out.println("Rodando....");
+	    System.out.println("Enviando mensagem...");
+		int randomNum = ThreadLocalRandom.current().nextInt(0, 35 + 1);
+		amqpTemplate.convertAndSend(exchange, routingkey, randomNum );
+
+	}
+}
